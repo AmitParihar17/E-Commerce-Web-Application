afd0dca0b8dccb7d7b03500bcd0f53bd04af7b8f
AmitParihar17 <amitparihar1208@gmail.com>
2025-07-14 00:22:53 +0530

    create add product page

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import {assets} from "../assets/assets.js"
 
-const Navbar = () => {
+const Navbar = ({setToken}) => {
   return (
     <div className='flex items-center justify-between py-2 px-[4%]'>
       <img className='w-[max(10%,80px)]' src={assets.logo} alt="Logo" />
-      <button className='bg-gray-600 text-white px-5 py-2 rounded-full sm:px-7 sm:py-2'>Logout</button>
+      <button onClick={() =>setToken("") } className='bg-gray-600 text-white px-5 py-2 rounded-full sm:px-7 sm:py-2 cursor-pointer'>Logout</button>
     </div>
   )
 }
